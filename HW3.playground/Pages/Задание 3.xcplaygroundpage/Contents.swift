06dd4a9c74a5bd7baa3e9ba500da5b0fad525feb
Yury Lebedev <quasaryy@gmail.com>
2023-08-04 05:43:55 +0400

    Initial Commit

---
+++ HW3.playground/Pages/Задание 3.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+/*:
+ ### Задание 3
+ 
+ 3.1 Определите квартал в котором вы родились, используя switch.
+ */
+let myBirthQuarter = 2
+
+switch myBirthQuarter {
+case 1:
+    print("i was born in the first quarter")
+case 2:
+    print("i was born in the second quarter")
+case 3:
+    print("i was born in the thirrd quarter")
+case 4:
+    print("i was born in the the first quarter")
+default:
+    ("error")
+}
+//: [Ранее: Задание 2](@previous)  |  задание 3 из 6  |  [Далее: Задание 4](@next)
